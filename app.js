88d9c2ba592c42abe1efe8688528f0d029c2c9e0
brettcomardelle <brettcomardelle93@gmail.com>
2021-04-23 11:44:23 -0500

    navigate to html page

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end("/clue.html");
 });
 
 server.listen(port, () => {
