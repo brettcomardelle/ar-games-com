8bcb23a7f559e4e6722f8c8d01ceb5b2cf4296c4
brettcomardelle <brettcomardelle93@gmail.com>
2021-04-23 14:30:45 -0500

    location AR app

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = process.env.PORT;
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/scripts'));
 app.get("*", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
