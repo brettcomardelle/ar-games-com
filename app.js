c778d899d2144d1e0e54080f8c19068c8dc3021c
brettcomardelle <brettcomardelle93@gmail.com>
2021-04-23 11:25:40 -0500

    add expressjs

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const https = require('https');
-const fs = require('fs');
+// var http = require('http');
+var express = require("express");
 
-const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-};
+var app = express();
 
-https.createServer(options, function (req, res) {
-  res.writeHead(200);
-  res.end("hello world\n");
-}).listen(8000);
+app.set('port', process.env.PORT || 3000);
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  console.log('[support dash] processing get request')
+  response.send('Hello World 2!');
+});
+
+app.listen(process.env.PORT, function () {
+  console.log('***** exp listening on port: ' + process.env.PORT);
+});
