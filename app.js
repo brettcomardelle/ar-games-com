71a3181e651742bfc51d3c6b65750036a6632465
brettcomardelle <brettcomardelle93@gmail.com>
2021-04-23 12:25:29 -0500

    fix routing

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile("index.html")//path.join(__dirname + '/index.html'));
 });
 // app.use('/', express.static('app', {index: "index.html"}));
