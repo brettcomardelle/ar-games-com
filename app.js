4b85f4c97839a87ddd30a746618a521f58dc30b1
brettcomardelle <brettcomardelle93@gmail.com>
2021-04-23 12:05:07 -0500

    server index

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello, world");
-});
+// app.get("/", (req, res) => {
+//   res.sendFile('index.html', { root: __dirname + "/relative_path_of_file" } );
+// });
+app.use('/', express.static('app', {index: "index.html"}));
 
 app.listen(port, () => {
   console.log("App is listening")
