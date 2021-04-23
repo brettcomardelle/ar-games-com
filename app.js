61616ce133fc5131a043df20624e459e3a40b2a5
brettcomardelle <brettcomardelle93@gmail.com>
2021-04-23 13:46:20 -0500

    fix routing

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require('path')
 const port = process.env.PORT;
 
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + '/home/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
-// app.use('/', express.static('app', {index: "index.html"}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log("App is listening")
