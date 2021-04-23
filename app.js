16d906eb39ca2104fffda2934d98991c599b2d09
brettcomardelle <brettcomardelle93@gmail.com>
2021-04-23 18:42:21 -0500

    adjust marker position

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = process.env.PORT;
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/scripts'));
+app.use(express.static(__dirname + '/assets'));
 app.get("*", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
